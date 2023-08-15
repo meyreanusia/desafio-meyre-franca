d113aaffaa790d56108243315e19833ccc8fd228
Meyre Anusia <meyreanusia@gmail.com>
2023-08-15 18:13:10 -0300

    teste

--- src/caixa-da-lanchonete.test.js
+++ src/caixa-da-lanchonete.test.js
@@ -9,29 +9,29 @@ describe('CaixaDaLanchonete', () => {
         expect(resultado.replace("\xa0", " ")).toEqual(resultadoEsperado);
     };
 
-    // test.each([
-    //     ['com carrinho vazio', 'dinheiro', 'Não há itens no carrinho de compra!', []],
-    //     ['com carrinho vazio', 'credito', 'Não há itens no carrinho de compra!', []],
-    //     ['com carrinho vazio', 'debito', 'Não há itens no carrinho de compra!', []],
-    // ])('compra %p em %p deve resultar em %p', (_, formaDePagamento, resultadoEsperado, itens) =>
-    //     validaTeste(formaDePagamento, resultadoEsperado, itens));
+    test.each([
+        ['com carrinho vazio', 'dinheiro', 'Não há itens no carrinho de compra!', []],
+        ['com carrinho vazio', 'credito', 'Não há itens no carrinho de compra!', []],
+        ['com carrinho vazio', 'debito', 'Não há itens no carrinho de compra!', []],
+    ])('compra %p em %p deve resultar em %p', (_, formaDePagamento, resultadoEsperado, itens) =>
+        validaTeste(formaDePagamento, resultadoEsperado, itens));
 
-    // test.each([
-    //     ['dinheiro', 'R$ 2,85', ['cafe,1']],
-    //     ['credito', 'R$ 3,09', ['cafe,1']],
-    //     ['debito', 'R$ 3,00', ['cafe,1']],
-    // ])('compra simples em %p deve resultar em %p', validaTeste);
+    test.each([
+        ['dinheiro', 'R$ 2,85', ['cafe,1']],
+        ['credito', 'R$ 3,09', ['cafe,1']],
+        ['debito', 'R$ 3,00', ['cafe,1']],
+    ])('compra simples em %p deve resultar em %p', validaTeste);
 
-    // test.each([
-    //     ['credito', 'R$ 11,85', ['cafe,1', 'sanduiche,1', 'queijo,1']],
-    //     ['debito', 'R$ 11,50', ['cafe,1', 'sanduiche,1', 'queijo,1']],
-    // ])('compra de 3 itens em %p deve resultar em %p', validaTeste);
+    test.each([
+        ['credito', 'R$ 11,85', ['cafe,1', 'sanduiche,1', 'queijo,1']],
+        ['debito', 'R$ 11,50', ['cafe,1', 'sanduiche,1', 'queijo,1']],
+    ])('compra de 3 itens em %p deve resultar em %p', validaTeste);
 
-    // test.each([
-    //     ['dinheiro', 'R$ 33,73', ['cafe,4', 'sanduiche,3', 'queijo,2']],
-    //     ['credito', 'R$ 36,56', ['cafe,4', 'sanduiche,3', 'queijo,2']],
-    //     ['debito', 'R$ 35,50', ['cafe,4', 'sanduiche,3', 'queijo,2']],
-    // ])('compra de múltiplas quantidades em %p deve resultar em %p', validaTeste);
+    test.each([
+        ['dinheiro', 'R$ 33,73', ['cafe,4', 'sanduiche,3', 'queijo,2']],
+        ['credito', 'R$ 36,56', ['cafe,4', 'sanduiche,3', 'queijo,2']],
+        ['debito', 'R$ 35,50', ['cafe,4', 'sanduiche,3', 'queijo,2']],
+    ])('compra de múltiplas quantidades em %p deve resultar em %p', validaTeste);
 
     test.each([
         ['com quantidade zero', 'dinheiro', 'Quantidade inválida!', ['cafe,0']],
